42bff7263f5debf08ac7416b49d8d93941e693b5
CraftLOL12 <134438564+CraftLOL12@users.noreply.github.com>
2023-11-24 16:02:49 +0300

    Add files via upload

---
+++ sorular.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+    //Kısa Sorular\\
+
+    soru1: "Günde Kaç Saat Aktif Olursun?",
+    örnekcevap1: "3",
+
+    soru2: "Kaç Yaşındasın?",
+    örnekcevap2: "16",
+
+    soru3: "Daha Önce Böyle Bir Sunucuda Bulundunmu?",
+    örnekcevap3: "Evet, Bulundum.",
+
+    soru4: "Günde Kaç Paylaşım Yapabilirsin?",
+    örnekcevap4: "6",
+
+    soru5: "Başkanlar Seni Çağırdığında Gelecekmisin?",
+    örnekcevap5: "Evet, Geleceğim.",
+
+    //Paragraf burdan sonrası\\
+
+    soru6: "Kuralları biliyon zaten Onaylıyonmu?",
+    örnekcevap6: "Evet, Onaylıyorum.",
+
+    soru7: "Söylemek İstediğin Birşey Varsa Yazabilirsin.",
+    örnekcevap7: "...",
+
+}
